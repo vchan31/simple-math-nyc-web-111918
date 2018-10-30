b0f1cdf39f3c6a54ab98ac7fe895a1bf7e7bf37c
sensible-proc-7557 <vchan313@gmail.com>
2018-10-30 13:40:30 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,13 +7,15 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  
+  num1 / num2
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  num1 % num2
 end
 
 def square_root(num)
